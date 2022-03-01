f410688bccf706afe81d22637e079689adb99620
Tobias Hladek <tobias.hladek@ententee.com>
2022-03-01 15:10:12 +0100

    Initial Commit

---
+++ Sygic/SygicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SygicApp.swift
+//  Sygic
+//
+//  Created by Tobiáš Hládek on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SygicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
